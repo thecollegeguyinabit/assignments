7cc81c953185ef1c20c9e2d92abf9b94fe1f5d9f
Hrushikesh Mohanta <12202040701053@mbit.edu.in>
2025-11-27 12:34:51 +0530

    Assignment 2

--- assignment-2/src/components/CardContainer.jsx
+++ assignment-2/src/components/CardContainer.jsx
@@ -1,3 +1,4 @@
+import { Badge } from "./ui/badge";
 import { 
     Card, 
     CardHeader,
@@ -9,7 +10,11 @@ export default function GameList({ platform, title, score, genre, editorChoice }
   return (
     <>
       <Card className=" w-[250px] md:max-lg:w-[280px] lg:w-[280px] bg-[#f1f2f1] h-auto gap-0 hover:cursor-pointer hover:shadow-2xl/30 hover:shadow-neutral-900/70 hover:-translate-y-2 ease-in-out delay-75">
-        <CardHeader className="h-auto mb-3">
+        <CardHeader className="h-auto mb-3 ">
+          {
+            editorChoice === "Y" &&
+          <Badge className="mb-2">Editor Choice</Badge>
+          }
           <CardTitle className="text-center text-wrap text-base/5">
             {title}
           </CardTitle>
@@ -32,12 +37,12 @@ export default function GameList({ platform, title, score, genre, editorChoice }
                 </div>
               )}
             </div>
-            <div className="mt-3 place-self-center">
+            {/* <div className="mt-3 place-self-center">
               {editorChoice === "Y" && (
                 <span className="px-2 py-1 rounded-full text-sm font-medium bg-neutral-700 text-white">
                     #Editor Choice
               </span>)}
-            </div>
+            </div> */}
           </div>
         </CardContent>
       </Card>
